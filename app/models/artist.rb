7e45ef1df4a086830453236a62841a1257526e0b
assouev <bsngr.asv@gmail.com>
2019-12-03 17:56:05 +0100

    devise/datavase updated

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 	has_many :likes
 	has_many :requests
 	belongs_to :group
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_153259) do
+ActiveRecord::Schema.define(version: 2019_12_03_162651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,14 @@ ActiveRecord::Schema.define(version: 2019_12_03_153259) do
     t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["group_id"], name: "index_artists_on_group_id"
+    t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
   end
 
   create_table "groups", force: :cascade do |t|
@@ -34,6 +41,13 @@ ActiveRecord::Schema.define(version: 2019_12_03_153259) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_groups_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_groups_on_reset_password_token", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
